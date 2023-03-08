c88a8c0e5bd1abb98dcf852d364bc37bc55475ca
Nitin vedwal <81376365+nitinvedwal@users.noreply.github.com>
2023-03-09 00:38:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 import statistics
 filelocation = "D:/UGP/data/data3/DS1_Modification_TimeFunctions/usuario1001/*"
 filelocation = glob.glob(filelocation)
-
+pie = 3.14159265359
 # read all signature of a person usuario1001
 # math.atan(x)
 for file in filelocation:
@@ -55,9 +55,9 @@ for file in filelocation:
         meanx = sum(x)/totalpoints
         meany = sum(y)/totalpoints
         meanpressure = sum(pressure)/totalpoints
-        print("mean x is"+str(meanx))
-        print("mean y is"+str(meany))
-        print("mean pressure is"+str(meanpressure))
+        # print("mean x is"+str(meanx))
+        # print("mean y is"+str(meany))
+        # print("mean pressure is"+str(meanpressure))
 
         stdsumx = 0  # (x^2-u)
         stdsumy = 0
@@ -72,9 +72,9 @@ for file in filelocation:
         stdy = math.sqrt(stdsumy/totalpoints)
         stdpressure = math.sqrt(stdsumpressure/totalpoints)
 
-        print("std x is "+str(stdx))
-        print("std y is"+str(stdy))
-        print("std pressure is"+str(stdpressure))
+        # print("std x is "+str(stdx))
+        # print("std y is"+str(stdy))
+        # print("std pressure is"+str(stdpressure))
         # print(timeseries)
 
         # ------------------------------section 2------------------------------------------------------
@@ -93,19 +93,23 @@ for file in filelocation:
             if (data[i][3]):
                 d.append(
                     math.sqrt(pow(data[i][0]-xmin, 2)+pow(data[i][1]-ymin, 2)))
-                theta.append(math.atan(data[i][1]/data[i][0]))
+                if (data[i][0]-xmin == 0):
+                    theta.append(pie/2)
+                else:
+                    theta.append(
+                        math.atan((data[i][1]-ymin)/(data[i][0]-xmin)))
 
         meand = sum(d)/totalpoints
         meantheta = sum(theta)/totalpoints
 
-        print("mean d is"+str(meand))
-        print("mean theta is"+str(meantheta))
+        # print("mean d is"+str(meand))
+        # print("mean theta is"+str(meantheta))
 
         mediand = statistics.median(d)
         mediantheta = statistics.median(theta)
 
-        print("meadin of d is"+str(mediand))
-        print("median of theta is"+str(mediantheta))
+        # print("meadin of d is"+str(mediand))
+        # print("median of theta is"+str(mediantheta))
 
         stdsumd = 0
         stdsumtheta = 0
@@ -116,9 +120,45 @@ for file in filelocation:
         stdd = math.sqrt(stdsumd/totalpoints)
         stdtheta = math.sqrt(stdsumtheta/totalpoints)
 
-        print("std d is"+str(stdd))
-        print("std theta is"+str(stdtheta))
-        
+        # print("std d is"+str(stdd))
+        # print("std theta is"+str(stdtheta))
 
         # ------------------------section 3----------------------------------------------
-        
+        # velocity acceleration angular velocity
+        tend = max(timeseries)
+        tstart = min(timeseries)
+
+        velocity = []
+        acceleration = []
+        angvelocity = []
+
+        for i in range(len(x)-1):
+            temp = (math.sqrt(pow(x[i+1]-x[i], 2) +
+                    pow(y[i+1]-y[i], 2)))*(tend-tstart)
+            velocity.append(temp/(timeseries[i+1]-timeseries[i]))
+            if (x[i+1] == 0 or x[i] == 0):
+                if (x[i+1] == 0 and x[i] == 0):
+                    temp1 = 0
+                elif (x[i+1] == 0 and x[i] != 0):
+                    temp1 = (abs(pie/2 -
+                                 math.atan(y[i]/x[i])))*(tend-tstart)
+                elif (x[i+1] != 0 and x[i] == 0):
+                    temp1 = (abs(math.atan(y[i+1]/x[i+1]) -
+                                 pie/2))*(tend-tstart)
+
+            else:
+                temp1 = (abs(math.atan(y[i+1]/x[i+1]) -
+                             math.atan(y[i]/x[i])))*(tend-tstart)
+            angvelocity.append(temp1/(timeseries[i+1]-timeseries[i]))
+
+        for i in range(len(velocity)-1):
+            temp = (abs(velocity[i+1]-velocity[i]))*(tend-tstart)
+            acceleration.append(temp/(timeseries[i+1]-timeseries[i]))
+
+        # -------------------------section 4-----------------------------------------------
+        # numbers of penups and down
+        pencount = 0
+        for i in range(len(data)-1):
+            if (data[i+1][3] != data[i][3]):
+                pencount += 1
+        print(pencount)
